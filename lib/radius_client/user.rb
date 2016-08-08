a6a8aad0393270f7e5af770c55f680a052a0dff7
ArturMinnullin <minnullin.artur@gmail.com>
2016-08-08 19:32:39 +0300

    Add placeholder for sign up

--- lib/radius_client/user.rb
+++ lib/radius_client/user.rb
@@ -1,6 +1,11 @@
 module RadiusClient
   class User
     class << self
+      def sign_up(name, password, options = {})
+        # psql -U radius -h 23.96.105.62
+        # INSERT INTO radcheck (username, attribute, op, value) VALUES (name, 'Cleartext-Password', ':=', password);
+      end
+
       def sign_in(name, password, options = {})
         reply = request.authenticate(name, password, options)
         Session.start(name, password, options) if reply.success?
